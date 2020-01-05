e1d09b46d9b92c67ba31b6fd95c188cb32a0ec9a
tkgstrator <nasawake.am@gmail.com>
2020-01-05 14:43:42 -0500

    First Commit for 0.2.0.

---
+++ lanplay_jp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo ./lan-play --fake-internet --pmtu 1000 --relay-server-addr tkgstrator.work:11451
---
+++ lanplay_na.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo ./lan-play --fake-internet --pmtu 1000 --relay-server-addr ec2-35-182-241-107.ca-central-1.compute.amazonaws.com:11451
