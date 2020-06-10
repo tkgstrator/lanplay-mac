4ccb7066ab7e728065107a6fb3a236a2426a9b51
tkgstrator <nasawake.am@gmail.com>
2020-06-10 00:56:48 -0400

    Update lanplay_na.sh

--- lanplay_na.sh
+++ lanplay_na.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo ./lan-play --fake-internet --pmtu 1000 --relay-server-addr ec2-35-182-241-107.ca-central-1.compute.amazonaws.com:11451
+sudo ./lan-play --fake-internet --pmtu 1000 --relay-server-addr shogi.mydns.jp:11451
