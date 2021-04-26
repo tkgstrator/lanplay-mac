daf11c016eef3d030283e656c4d020f23d1da8db
tkgstrator <nasawake.am@gmail.com>
2021-04-26 22:00:35 +0900

    Update lanplay_jp.sh

--- lanplay_jp.sh
+++ lanplay_jp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo ./lan-play --fake-internet --pmtu 1000 --relay-server-addr tkgstrator.work:11451
+sudo ./lan-play --fake-internet --pmtu 1000 --relay-server-addr ec2-13-231-102-57.ap-northeast-1.compute.amazonaws.com/:11451
